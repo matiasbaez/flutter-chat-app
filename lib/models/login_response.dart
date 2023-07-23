30366979fbf0b985366856eb4fe7287e91627499
Matías Báez <matiasbaez2512@gmail.com>
2023-07-23 03:24:22 -0400

    Added login_response model and updated user model

---
+++ lib/models/login_response.dart
@@ -0,0 +1,33 @@
+
+import 'dart:convert';
+
+import 'package:chat/models/models.dart';
+
+LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.decode(str));
+String loginResponseToJson(LoginResponse data) => json.encode(data.toJson());
+
+class LoginResponse {
+
+  bool ok;
+  User user;
+  String token;
+
+  LoginResponse({
+    required this.ok,
+    required this.user,
+    required this.token,
+  });
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
+    ok: json["ok"],
+    user: json["user"],
+    token: json["token"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "ok": ok,
+    "user": user,
+    "token": token,
+  };
+
+}
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,11 @@
 
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+String userToJson(User data) => json.encode(data.toJson());
+
 class User {
+
   bool online;
   String email;
   String name;
@@ -11,4 +17,18 @@ class User {
     required this.name,
     required this.uid
   });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    online: json["online"],
+    email: json["email"],
+    name: json["name"],
+    uid: json["uid"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "online": online,
+    "email": email,
+    "name": name,
+    "uid": uid,
+  };
 }
