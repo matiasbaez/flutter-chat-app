cfd98034192414084c1f986e2e35bfca40e4eb83
Matías Báez <matiasbaez2512@gmail.com>
2023-07-23 03:21:49 -0400

    Added environment.dart

---
+++ lib/global/environment.dart
@@ -0,0 +1,7 @@
+
+import 'dart:io';
+
+class Environment {
+  static String apiURL = Platform.isAndroid ? 'http://10.0.0.2:3000/api' : 'http://localhost:3000/api';
+  static String socketURL = Platform.isAndroid ? 'http://10.0.0.2:3000' : 'http://localhost:3000';
+}
